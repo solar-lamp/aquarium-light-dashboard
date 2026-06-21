ff20d478dfa4387c0d90de59a1ffa0d991805291
Souradeep Dey <129890493+solar-lamp@users.noreply.github.com>
2026-06-21 16:12:32 +0530

    Implement MQTT client for aquarium dashboard

--- script.js
+++ script.js
@@ -1,1 +1,43 @@
-console.log("Aquarium Dashboard");
+const options = {
+    host: "8f4859d367254182889b312a863177b0.s1.eu.hivemq.cloud",
+    port: 8884,
+    protocol: "wss",
+    path: "/mqtt",
+
+    username: "aquarium_admin",
+    password: "sdey123A"
+};
+
+const client = mqtt.connect(options);
+
+client.on("connect", () => {
+
+    console.log("Connected");
+
+    client.subscribe("aquarium/status");
+});
+
+client.on("message", (topic, message) => {
+
+    const data =
+        JSON.parse(
+            message.toString());
+
+    document.getElementById("online").innerText =
+        data.online;
+
+    document.getElementById("mode").innerText =
+        data.mode;
+
+    document.getElementById("light").innerText =
+        data.light;
+
+    document.getElementById("time").innerText =
+        data.time;
+
+    document.getElementById("slot1").innerText =
+        data.slot1;
+
+    document.getElementById("slot2").innerText =
+        data.slot2;
+});
